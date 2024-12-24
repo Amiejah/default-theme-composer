14223efb981029c0996a834cdcfe80b63ab6e8f8
Amir <info@humpff.com>
2024-12-24 11:29:42 +0100

    Updated

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import path from 'path';
+
+const ROOT = path.resolve('../../../');
+const BASE = __dirname.replace(ROOT, '');
+
+export default defineConfig({
+    base: process.env.NODE_ENV === 'production' ? `${BASE}/assets/build/` : BASE,
+    plugins: [
+        laravel({
+            input: [
+                'resources/styles/main.css',
+                'resources/scripts/main.js'
+            ],
+            publicDirectory: 'assets',
+            refresh: true,
+        }),
+    ],
+    server: {
+        hmr: {
+            host: 'localhost',
+        }
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources'),
+        },
+    },
+    build: {
+        assetsDir: '.',
+    },
+});
